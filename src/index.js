ae5857c980d74a54703fa9b6a7aba3ffb84294ce
Lucia Naveira <naveiralucia@gmail.com>
2023-09-15 18:38:56 -0300

    a

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const express = require('express')
 
 const app = express()
 
-app.get('/', (req, res) => res.send('hello world'))
+app.get('/', (req, res) => res.send('server is listening'))
 
 app.listen(3000)
 console.log('server is listening on port', 3000)
