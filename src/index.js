1468bce3e669ab7cae7ba512026734c2565b511c
Lucia Naveira <naveiralucia@gmail.com>
2023-09-15 19:27:24 -0300

    conecte a mysql

--- src/index.js
+++ src/index.js
@@ -1,9 +1,28 @@
+const express = require('express');
+const mysql = require('mysql');
+const app = express();
 
-const express = require('express')
 
-const app = express()
+const db = mysql.createConnection({
+  host: '127.0.0.1', 
+  user: 'root', 
+  database: 'DELIVERAR'
+});
 
-app.get('/', (req, res) => res.send('server is listening'))
 
-app.listen(3000)
-console.log('server is listening on port', 3000)
+db.connect((err) => {
+  if (err) {
+    console.error('Error connecting to MySql:', err);
+  } else {
+    console.log('Successfully connected to MySql');
+  }
+});
+
+app.get('/', (req, res) => {
+    res.send('Server is listening');
+  });
+
+
+app.listen(3000, () => {
+  console.log('server is listening on port', 3000);
+});
