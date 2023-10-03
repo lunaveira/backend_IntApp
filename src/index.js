a913c7f54208ad67194d5cc97bf28f9f78c24c39
Lucia Naveira <naveiralucia@gmail.com>
2023-10-03 10:58:13 -0300

    agregue metodo put users

--- src/index.js
+++ src/index.js
@@ -18,6 +18,35 @@ db.connect((err) => {
   }
 });
 
+app.use(express.json());
+
+
+
+
+app.put('/users/:id', (req, res) => {
+  const userId = req.params.id; 
+  const { name, lastname, email, password, dni, birth_date, gender, status } = req.body;
+
+  const updateUserQuery = `
+    UPDATE users 
+    SET name = ?, lastname = ?, email = ?, password = ?, dni = ?, birth_date = ?, gender = ?, status = ? 
+    WHERE id = ?
+  `;
+
+  const values = [name, lastname, email, password, dni, birth_date, gender, status, userId];
+
+  db.query(updateUserQuery, values, (err, result) => {
+    if (err) {
+      console.error('Error updating user:', err);
+      res.status(500).json({ error: 'Error updating user' });
+    } else {
+      console.log('User updated successfully');
+      res.status(200).json({ message: 'User updated successfully' });
+    }
+  });
+});
+
+
 app.get('/', (req, res) => {
     res.send('Server is listening');
   });
